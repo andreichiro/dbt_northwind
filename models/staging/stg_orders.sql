e5b390ff1cfa242f4b645b3057d2bc9dbbd0db74
andreichiro@gmail.com <andreichiro@gmail.com>
2023-05-25 22:11:27 +0000

    Materialized incremental

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,9 +1,13 @@
+{{config(materialized='incremental')}}
+
 with 
     orders as (
-        select 
-            *
+        select *
         from {{source('northwind', 'orders')}}
-    )
+        {% if is_incremental() %}
+        where order_date > (select max(order_date) from {{this}})
+        {% endif %}
+)
 
 select * 
 from orders
